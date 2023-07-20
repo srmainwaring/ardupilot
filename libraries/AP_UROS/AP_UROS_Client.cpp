7d30645ffbd7a6b2e4a8e42d1fa81b7dffa34877
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-07-20 12:34:04 +0100

    AP_UROS: add vector3 subscriber example [skip ci]
    - PoC - will only work for SITL / POSIX (must add use custom transport)
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_UROS/AP_UROS_Client.cpp
+++ libraries/AP_UROS/AP_UROS_Client.cpp
@@ -12,6 +12,42 @@
 
 #include "AP_UROS_Client.h"
 
+// micro-ROS
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int32.h>
+#include <geometry_msgs/msg/vector3.h>
+
+#define RCCHECK(fn) {\
+  rcl_ret_t temp_rc = fn;\
+  if ((temp_rc != RCL_RET_OK)) {\
+    printf("Failed status on line %d: %d. Aborting.\n", \
+        __LINE__, (int)temp_rc);\
+  } \
+}
+
+#define RCSOFTCHECK(fn) {\
+  rcl_ret_t temp_rc = fn;\
+  if ((temp_rc != RCL_RET_OK)) {\
+    printf("Failed status on line %d: %d. Continuing.\n", \
+        __LINE__, (int)temp_rc);\
+  }\
+}
+
+rcl_subscription_t subscriber;
+geometry_msgs__msg__Vector3 msg;
+
+void subscription_callback(const void * msgin)
+{
+    const geometry_msgs__msg__Vector3 * pmsg =
+        (const geometry_msgs__msg__Vector3 *)msgin;
+    GCS_SEND_TEXT(MAV_SEVERITY_INFO,"UROS: x: %f, y: %f, z: %f",
+        pmsg->x, pmsg->y, pmsg->z);
+}
+
 const AP_Param::GroupInfo AP_UROS_Client::var_info[] {
 
     // @Param: _ENABLE
@@ -57,6 +93,38 @@ void AP_UROS_Client::main_loop(void)
     }
     GCS_SEND_TEXT(MAV_SEVERITY_INFO,"UROS: initialization passed");
 
+    // one-time actions
+
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rclc_support_t support;
+
+    // create init_options
+    RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+    // create node
+    rcl_node_t node;
+    RCCHECK(rclc_node_init_default(
+        &node, "vector3_subscriber_rclc", "", &support));
+
+    // create subscriber
+    RCCHECK(rclc_subscription_init_default(
+        &subscriber,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3),
+        "geometry_msgs_msg_Vector3"));
+
+    // create executor
+    rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_add_subscription(
+        &executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+
+    rclc_executor_spin(&executor);
+
+    RCCHECK(rcl_subscription_fini(&subscriber, &node));
+    RCCHECK(rcl_node_fini(&node));
+
+    // periodic actions
     while (true) {
         hal.scheduler->delay(1);
         update();
