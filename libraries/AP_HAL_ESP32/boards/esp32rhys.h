91919e383022a5dc497c665ccd42d22d68ed1fc7
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-08-27 20:26:46 +0100

    AP_HAL_ESP32: update board definition esp32rhys
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_HAL_ESP32/boards/esp32rhys.h
+++ libraries/AP_HAL_ESP32/boards/esp32rhys.h
@@ -37,14 +37,22 @@
 //#define DEFAULT_SERIAL1_PROTOCOL        SerialProtocol_MAVLink2   //C  WiFi:  TCP, UDP, or disable (depends on HAL_ESP32_WIFI)
 //#define DEFAULT_SERIAL1_BAUD            AP_SERIALMANAGER_MAVLINK_BAUD/1000  //57600
 
-// #define DEFAULT_SERIAL2_PROTOCOL        SerialProtocol_MAVLink2   //D  UART2
-// #define DEFAULT_SERIAL2_BAUD            AP_SERIALMANAGER_MAVLINK_BAUD/1000  //57600
-#define DEFAULT_SERIAL2_PROTOCOL        SerialProtocol_DDS_XRCE   //D  UART2
-#define DEFAULT_SERIAL2_BAUD            115200/1000  //57600
+#define DEFAULT_SERIAL2_PROTOCOL        SerialProtocol_MAVLink2   //D  UART2
+#define DEFAULT_SERIAL2_BAUD            AP_SERIALMANAGER_MAVLINK_BAUD/1000  //57600
+//#define DEFAULT_SERIAL2_PROTOCOL        SerialProtocol_DDS_XRCE   //D  UART2
+//#define DEFAULT_SERIAL2_BAUD            (115200/1000)
+//#define DEFAULT_SERIAL2_PROTOCOL        SerialProtocol_MSP        //D  UART2
+//#define DEFAULT_SERIAL2_BAUD            (115200/1000)
+//#define DEFAULT_SERIAL2_PROTOCOL        SerialProtocol_None       //D  UART2
+//#define DEFAULT_SERIAL2_BAUD            (115200/1000)
 
-#define DEFAULT_SERIAL3_PROTOCOL        SerialProtocol_GPS        //B  UART1: GPS1
-#define DEFAULT_SERIAL3_BAUD            AP_SERIALMANAGER_GPS_BAUD/1000    //38400, Can not define default baudrate here (by config only)
-//#define DEFAULT_SERIAL3_PROTOCOL        SerialProtocol_None       //B
+//#define DEFAULT_SERIAL3_PROTOCOL        SerialProtocol_GPS        //B  UART1
+//#define DEFAULT_SERIAL3_BAUD            AP_SERIALMANAGER_GPS_BAUD/1000
+//#define DEFAULT_SERIAL3_PROTOCOL        SerialProtocol_MAVLink2   //B  UART1
+//#define DEFAULT_SERIAL3_BAUD            (AP_SERIALMANAGER_MAVLINK_BAUD/1000)
+#define DEFAULT_SERIAL3_PROTOCOL        SerialProtocol_MSP        //B  UART1
+#define DEFAULT_SERIAL3_BAUD            (115200/1000)
+//#define DEFAULT_SERIAL3_PROTOCOL        SerialProtocol_None       //B  UART1
 //#define DEFAULT_SERIAL3_BAUD            (115200/1000)
 
 #define DEFAULT_SERIAL4_PROTOCOL        SerialProtocol_None       //E
@@ -65,14 +73,16 @@
 #define DEFAULT_SERIAL9_PROTOCOL        SerialProtocol_None       //J
 #define DEFAULT_SERIAL9_BAUD            (115200/1000)
 
-//Inertial sensors
+//Probe for sensors
+#define PROBE_IMU_I2C(driver, bus, addr, args ...) ADD_BACKEND(AP_InertialSensor_ ## driver::probe(*this, GET_I2C_DEVICE(bus, addr), ##args))
+
+//INS choices:
 #define HAL_INS_DEFAULT HAL_INS_NONE
-//#define HAL_INS_DEFAULT HAL_INS_MPU9250_I2C
-//#define PROBE_IMU_I2C(driver, bus, addr, args ...) ADD_BACKEND(AP_InertialSensor_ ## driver::probe(*this,GET_I2C_DEVICE(bus, addr),##args))
-//#define HAL_INS_PROBE_LIST PROBE_IMU_I2C(Invensense, 0, 0x68, ROTATION_NONE)
+//#define HAL_INS_DEFAULT HAL_INS_BNO080_I2C
 
 //I2C Buses
-#define HAL_ESP32_I2C_BUSES {.port=I2C_NUM_0, .sda=GPIO_NUM_13, .scl=GPIO_NUM_14, .speed=400*KHZ, .internal=true, .soft=true}
+#define HAL_ESP32_I2C_BUSES \
+  {.port=I2C_NUM_0, .sda=GPIO_NUM_21, .scl=GPIO_NUM_22, .speed=400*KHZ, .internal=true, .soft=true}
 
 //SPI Buses
 #define HAL_ESP32_SPI_BUSES {}
@@ -98,12 +108,8 @@
 // #define AP_INERTIALSENSOR_ENABLED 1
 // #define AP_INERTIALSENSOR_KILL_IMU_ENABLED 0
 
-//IMU
-// #define AP_INERTIALSENSOR_ENABLED 1
-// #define AP_INERTIALSENSOR_KILL_IMU_ENABLED 0
-
 //COMPASS
-#define AP_COMPASS_ENABLE_DEFAULT 0
+#define AP_COMPASS_ENABLE_DEFAULT 1
 #define ALLOW_ARM_NO_COMPASS
 
 //See boards.py
@@ -122,6 +128,9 @@
 
 //UARTs
 // UART_NUM_0 and UART_NUM_2 are configured to use defaults
+// UART_NUM_0 => SERIAL0
+// UART_NUM_1 => SERIAL3
+// UART_NUM_2 => SERIAL2
 #define HAL_ESP32_UART_DEVICES \
     {.port=UART_NUM_0, .rx=GPIO_NUM_3 , .tx=GPIO_NUM_1 },\
     {.port=UART_NUM_1, .rx=GPIO_NUM_34, .tx=GPIO_NUM_18},\
