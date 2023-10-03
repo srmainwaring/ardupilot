79c879784458b81fa43b2057f18b4f4710d4c929
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-10-03 21:25:23 +0100

    AP_ROS: add mutable versions of accessor templates
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_ROS/AP_ROS_TypeConversions.h
+++ libraries/AP_ROS/AP_ROS_TypeConversions.h
@@ -24,17 +24,37 @@ template <typename S>
 const char* string_data(const S* str);
 
 template <typename S>
+char* mutable_string_data(S* str);
+
+template <typename S>
 const char* string_data(const S& str);
+
+template <typename S>
+char* mutable_string_data(S& str);
 
 // sequence accessor templates
 // see: https://stackoverflow.com/questions/15911890/overriding-return-type-in-function-template-specialization
 
 template <typename T>
-uint32_t transforms_size(const T& msg);
+struct transforms_size_type{ typedef uint32_t type; };
 
 template <typename T>
-struct transforms_type{ typedef T* type; };
+struct mutable_transforms_size_type{ typedef uint32_t& type; };
+
+template <typename T>
+typename transforms_size_type<T>::type transforms_size(const T& msg);
+
+template <typename T>
+typename mutable_transforms_size_type<T>::type transforms_mutable_size(T& msg);
+
+template <typename T>
+struct transforms_type{ typedef void* type; };
+
+template <typename T>
+struct mutable_transforms_type{ typedef void* type; };
 
 template <typename T>
 typename transforms_type<T>::type transforms_data(const T& msg);
 
+template <typename T>
+typename mutable_transforms_type<T>::type transforms_mutable_data(T& msg);
