3fc3f2b286226ce2256fead3141c09217ebe4b91
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-10-03 13:10:42 +0100

    AP_UROS: use common code from AP_ROS
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_UROS/AP_UROS_Type_Conversions.cpp
+++ libraries/AP_UROS/AP_UROS_Type_Conversions.cpp
@@ -1,12 +1,14 @@
 #include "AP_UROS_Type_Conversions.h"
 #if AP_UROS_ENABLED
 
+#include <AP_ROS/AP_ROS_TypeConversions.h>
+
 #include <builtin_interfaces/msg/time.h>
 
 
 uint64_t AP_UROS_Type_Conversions::time_u64_micros(const builtin_interfaces__msg__Time& ros_time)
 {
-    return (uint64_t(ros_time.sec) * 1000000ULL) + (ros_time.nanosec / 1000ULL);
+    return AP_ROS_TypeConversions::time_u64_micros(ros_time);
 }
 
 
