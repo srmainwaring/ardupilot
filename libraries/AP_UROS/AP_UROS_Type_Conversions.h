0b14127356b8de876ecd2d848617c11a968819e0
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-10-04 00:23:22 +0100

    AP_UROS: move template specialisation definitions to cpp to prevent duplicate symbols
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_UROS/AP_UROS_Type_Conversions.h
+++ libraries/AP_UROS/AP_UROS_Type_Conversions.h
@@ -20,58 +20,46 @@ public:
 
 // string specialisations
 template <>
-const char* string_data(const rosidl_runtime_c__String& str) {
-    return str.data;
-}
+const char* string_data(const rosidl_runtime_c__String& str);
 
 template <>
-char* mutable_string_data(rosidl_runtime_c__String& str) {
-    return str.data;
-}
+char* mutable_string_data(rosidl_runtime_c__String& str);
 
 // transform specialisations
 template <>
-struct transforms_size_type<tf2_msgs__msg__TFMessage>{
+struct transforms_size_type<tf2_msgs__msg__TFMessage> {
     typedef size_t type;
 };
 
 template <>
-struct mutable_transforms_size_type<tf2_msgs__msg__TFMessage>{
+struct mutable_transforms_size_type<tf2_msgs__msg__TFMessage> {
     typedef size_t& type;
 };
 
 template <>
 typename transforms_size_type<tf2_msgs__msg__TFMessage>::type
-transforms_size(const tf2_msgs__msg__TFMessage& msg) {
-    return msg.transforms.size;
-}
+transforms_size(const tf2_msgs__msg__TFMessage& msg);
 
 template <>
 typename mutable_transforms_size_type<tf2_msgs__msg__TFMessage>::type
-mutable_transforms_size(tf2_msgs__msg__TFMessage& msg) {
-    return msg.transforms.size;
-}
+mutable_transforms_size(tf2_msgs__msg__TFMessage& msg);
 
 template <>
-struct transforms_type<tf2_msgs__msg__TFMessage>{
+struct transforms_type<tf2_msgs__msg__TFMessage> {
     typedef const geometry_msgs__msg__TransformStamped* type;
 };
 
 template <>
-struct mutable_transforms_type<tf2_msgs__msg__TFMessage>{
+struct mutable_transforms_type<tf2_msgs__msg__TFMessage> {
     typedef geometry_msgs__msg__TransformStamped* type;
 };
 
 template <>
 typename transforms_type<tf2_msgs__msg__TFMessage>::type
-transforms_data(const tf2_msgs__msg__TFMessage& msg) {
-    return msg.transforms.data;
-}
+transforms_data(const tf2_msgs__msg__TFMessage& msg);
 
 template <>
 typename mutable_transforms_type<tf2_msgs__msg__TFMessage>::type
-mutable_transforms_data(tf2_msgs__msg__TFMessage& msg) {
-    return msg.transforms.data;
-}
+mutable_transforms_data(tf2_msgs__msg__TFMessage& msg);
 
 #endif // AP_UROS_ENABLED
