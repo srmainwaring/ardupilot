e0d3abdc4e19b23600d4515a489b0e7495fb4acc
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-10-03 13:10:00 +0100

    AP_ROS: factor out common code from AP_DDS and AP_UROS
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ libraries/AP_ROS/AP_ROS_TypeConversions.h
@@ -0,0 +1,20 @@
+// Class for handling type conversions for ROS.
+
+#pragma once
+
+#include <stdint.h>
+
+class AP_ROS_TypeConversions
+{
+public:
+
+    // Convert ROS time to a uint64_t [μS]
+    template<typename TTime>
+    static uint64_t time_u64_micros(const TTime& ros_time);
+};
+
+template<typename TTime>
+uint64_t AP_ROS_TypeConversions::time_u64_micros(const TTime& ros_time)
+{
+    return (uint64_t(ros_time.sec) * 1000000ULL) + (ros_time.nanosec / 1000ULL);
+}
