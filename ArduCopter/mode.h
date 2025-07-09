632ff37db391172520831390d48a94fdca03c19f
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-07-09 19:35:43 +0100

    Copter: post rebase fixes
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -2052,7 +2052,7 @@ public:
     bool allows_arming(AP_Arming::Method method) const override { return false; };
 
     // Functions to support NAV_CONTROLLER_OUTPUT telemetry
-    int32_t wp_bearing() const override;
+    int32_t wp_bearing() const;
     float wp_distance_m() const override;
     float crosstrack_error() const override;
 
